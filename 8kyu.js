f3cad6548e4401b22645bfaa03444a25de175525
shox2552-png <shox2552@gmail.com>
2025-12-19 16:23:10 +0500

    changet

--- 8kyu.js
+++ 8kyu.js
@@ -3,15 +3,15 @@
 //  console.log(newArr);
 
 
-let numbers = [1, 5.2, 4, 0, -1]
+// let numbers = [1, 5.2, 4, 0, -1]
 
-function sum (numbers) {
-    if (numbers.length === 0) return 0;
+// function sum (numbers) {
+//     if (numbers.length === 0) return 0;
     
-     let summa = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      summa += numbers[i];
-    }
-    return summa
-  }
-console.log();
+//      let summa = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//       summa += numbers[i];
+//     }
+//     return summa
+//   }
+// console.log();
