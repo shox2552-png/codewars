1227645e7e1bbf07ed0f01fb9d3326e346cffbb6
shox2552-png <shox2552@gmail.com>
2025-12-19 16:15:52 +0500

    comentariya

--- 8kyu.js
+++ 8kyu.js
@@ -1,3 +1,17 @@
-let arr = [1, 2, 3]
-let newArr = arr.map((n)=> n*2 )
- console.log(newArr);
+// let arr = [1, 2, 3]
+// let newArr = arr.map((n)=> n*2 )
+//  console.log(newArr);
+
+
+let numbers = [1, 5.2, 4, 0, -1]
+
+function sum (numbers) {
+    if (numbers.length === 0) return 0;
+    
+     let summa = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      summa += numbers[i];
+    }
+    return summa
+  }
+console.log();
