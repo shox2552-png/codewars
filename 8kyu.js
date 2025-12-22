5a80448bbef597a4008b513318b85ce8b918a6cc
shox2552-png <shox2552@gmail.com>
2025-12-22 20:43:51 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -45,3 +45,12 @@
 // let newArr = arr.reduce((a,b) =>  a*b,1 )
 
 // console.log(newArr);
+
+
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// let newArr = arr.filter((num) => {
+//    return  num = num % 2 === 0 
+// })
+
+// console.log(newArr);
