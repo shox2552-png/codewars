56c3c97eb0b9d17f6cec30431847f6ddef3e8137
shox2552-png <shox2552@gmail.com>
2025-12-27 19:02:49 +0500

    changet

--- 8kyu.js
+++ 8kyu.js
@@ -107,3 +107,14 @@
 // function opposite(number) {
 //     return -number
 // }
+
+let arr = [1, -4, 7, 12]
+
+let sum = 0
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        sum += arr[i]
+    }
+}
+console.log(sum);
