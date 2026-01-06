d0a2c5719ad8d02255c0db292d6c43bfce835c0e
shox2552-png <shox2552@gmail.com>
2026-01-06 16:51:46 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -152,3 +152,13 @@
 
 
 // let sortList = (sortBy, list)=> list.sort((a, b) => b[sortBy] - a[sortBy])
+
+function consecutive(array) {
+  if(array.length<=1)   return 0;
+    let min = Math.min(...array);
+    let max = Math.max(...array);
+  return(max - min +1)-array.length
+}
+console.log(array);
+
+
