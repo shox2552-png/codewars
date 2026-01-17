ab78bae2f7ddb1b76c19566b694b34ae08fae9c9
shox2552-png <shox2552@gmail.com>
2026-01-17 17:57:57 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -186,3 +186,8 @@
 // function stringy(size, a='') {
 //    for (let i = 0; i < size; i++) {  a += i % 2 === 0 ? '1' : '0';} return a}
 // let stringy = (size)=> Array(size).fill(0).map((v,i)=>(i+1)%2?1:0).join("");
+// let gooseFilter = (arr)=> arr = arr.filter((v)=> !["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"].includes(v))
+
+// function smash (words) {
+//    return words.join(" ");
+// };
