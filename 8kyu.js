d8655501c03756f511fe5d85d5fd97de8796470a
shox2552-png <shox2552@gmail.com>
2026-01-19 15:49:19 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -195,3 +195,6 @@
 // function arrayPlusArray(arr1, arr2) {
 //   return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
 // }
+
+// Array.prototype.first = function() { return this[0] }
+// Array.prototype.last  = function() { return this[this.length - 1] }
