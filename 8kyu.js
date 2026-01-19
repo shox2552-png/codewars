d6829508c2cc76a530db75f58b61f3e03275c5a2
shox2552-png <shox2552@gmail.com>
2026-01-19 20:13:14 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -198,3 +198,7 @@
 
 // Array.prototype.first = function() { return this[0] }
 // Array.prototype.last  = function() { return this[this.length - 1] }
+
+// function duckDuckGoose(players, goose) {
+//   return players[(goose - 1) % players.length].name;
+// }
