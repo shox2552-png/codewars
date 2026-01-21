a8f177e164b62d82fd46cca985cf5828f53a41dc
shox2552-png <shox2552@gmail.com>
2026-01-21 21:36:56 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -210,3 +210,12 @@
 // function doubleInteger(i) {
 //   return i*2;
 // }
+
+// let add = (n1, n2, max = 0, str = "") => {
+//   n1 > n2 ? (max = n1) : (max = n2);
+//   let arr1 = `${n1}`.split("");
+//   let arr2 = `${n2}`.split("");
+//   for (let i = 1; i <= `${max}`.length; i++)
+//     str = (+arr1.at(-i) || 0) + (+arr2.at(-i) || 0) + str;
+//   return +str;
+// };
